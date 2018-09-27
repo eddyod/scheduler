9c22f54a72ea1fcb5dbb4d090eb48faa5ec0fb06
Eddy O'Donnell <eddyod@yahoo.com>
2018-09-27 07:32:13 -0700

    Added calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FlatpickrModule} from 'angularx-flatpickr';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -13,10 +15,11 @@ import {SchoolCreateComponent} from './school-create/school-create.component';
 import {ScheduleListComponent} from './schedule-list/schedule-list.component';
 import {ScheduleCreateComponent} from './schedule-create/schedule-create.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import { MycalComponent } from './mycal/mycal.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CalendarModule, DateAdapter} from 'angular-calendar';
+import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
+import {NgbModalModule} from '@ng-bootstrap/ng-bootstrap';
+import {MycalComponent} from './mycal/mycal.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +36,10 @@ import { MycalComponent } from './mycal/mycal.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    CommonModule,
     FormsModule,
+    NgbModalModule,
+    FlatpickrModule.forRoot(),
     ReactiveFormsModule,
     BrowserAnimationsModule,
     CalendarModule.forRoot({
--- src/app/mycal/mycal.component.ts
+++ src/app/mycal/mycal.component.ts
@@ -1,18 +1,67 @@
 import 'angular-calendar/css/angular-calendar.css';
-import { Component, OnInit } from '@angular/core';
+import {Component, ChangeDetectionStrategy, ViewChild, TemplateRef} from '@angular/core';
+import {CalendarEvent} from 'angular-calendar';
+
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA'
+  }
+};
+
 
 
 @Component({
   selector: 'app-mycal',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './mycal.component.html',
   styleUrls: ['./mycal.component.css']
 })
-export class MycalComponent implements OnInit {
+export class MycalComponent {
+  view = 'month';
+
   viewDate: Date = new Date();
-  events = [];
-  constructor() { }
 
-  ngOnInit() {
-  }
-
+  events: CalendarEvent[] = [
+    {
+      title: 'Editable event',
+      color: colors.yellow,
+      start: new Date(),
+      actions: [
+        {
+          label: '<i class="fa fa-fw fa-pencil"></i>',
+          onClick: ({event}: {event: CalendarEvent}): void => {
+            console.log('Edit event', event);
+          }
+        }
+      ]
+    },
+    {
+      title: 'Deletable event',
+      color: colors.blue,
+      start: new Date(),
+      actions: [
+        {
+          label: '<i class="fa fa-fw fa-times"></i>',
+          onClick: ({event}: {event: CalendarEvent}): void => {
+            this.events = this.events.filter(iEvent => iEvent !== event);
+            console.log('Event deleted', event);
+          }
+        }
+      ]
+    },
+    {
+      title: 'Non editable and deletable event',
+      color: colors.red,
+      start: new Date()
+    }
+  ];
 }
