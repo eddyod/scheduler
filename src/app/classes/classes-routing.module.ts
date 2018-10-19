e487aebc568badea2d5a5b6c30ec7f93c892dbc1
Edward O'Donnell <eodonnell@ucsd.edu>
2018-10-19 14:04:59 -0700

    classes working

--- src/app/classes/classes-routing.module.ts
+++ src/app/classes/classes-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClassListComponent } from './class-list/class-list.component';
+
 const routes: Routes = [
-  {path: 'classes', component: ClassListComponent}
+  {path: 'classes', component: ClassListComponent},
+  {path: '**', redirectTo: 'classes'}
 ];
 
 @NgModule({
