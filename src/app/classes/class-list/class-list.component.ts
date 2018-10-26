206aa841e59d104ab0146c576f81266f59f3746f
Edward O'Donnell <eodonnell@ucsd.edu>
2018-10-26 16:05:30 -0700

    Mat table
    1. Added selection to row
    2. Added delete button to display selected rows.

--- src/app/classes/class-list/class-list.component.ts
+++ src/app/classes/class-list/class-list.component.ts
@@ -1,18 +1,25 @@
 import { Component, ViewChild } from '@angular/core';
 import { ScheduleEvent } from '../../models/scheduleEvent';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { SelectionModel } from '@angular/cdk/collections';
 
 import { APIService } from '../../services/api.service';
+
+const initialSelection = [];
+const allowMultiSelect = true;
+
 
 @Component({
   selector: 'app-class-list',
   templateUrl: './class-list.component.html',
   styleUrls: ['./class-list.component.css']
 })
-export class ClassListComponent  {
+export class ClassListComponent {
 
-  displayedColumns = ['start', 'end', 'completed', 'teacher',  'school'];
+  displayedColumns = ['start', 'end', 'completed', 'teacher', 'school', 'select'];
   dataSource: MatTableDataSource<ScheduleEvent>;
+  selection = new SelectionModel<ScheduleEvent>(allowMultiSelect, initialSelection);
+
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -39,6 +46,25 @@ export class ClassListComponent  {
     filterValue = filterValue.trim(); // Remove whitespace
     filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
     this.dataSource.filter = filterValue;
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected == numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  checkDelete() {
+    console.log(this.selection.selected.forEach(row => console.log(row)));
+    this.selection.clear();
   }
 
 
